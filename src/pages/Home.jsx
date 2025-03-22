d241191e10e2b95fa7c87ce936acb78231d016b2
Hrid1402 <Hrid1402@gmail.com>
2025-03-22 14:01:59 -0500

    fix: import error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import styles from '../styles/home.module.scss'
-import NavBar from '../components/navBar';
+import NavBar from '../components/NavBar';
 import Presentation from '../components/Presentation';
 import Projects from '../components/Projects';
 import TechStack from '../components/TechStack';
